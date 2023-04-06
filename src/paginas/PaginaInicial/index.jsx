ae1fef480c8e8fd2f1f8babab298377f3514b7cf
jobemcamera <jobecamera@gmail.com>
2023-04-06 17:48:15 -0300

    estilização do componente Populares

--- src/paginas/PaginaInicial/index.jsx
+++ src/paginas/PaginaInicial/index.jsx
@@ -1,27 +1,29 @@
 import Cabecalho from "../../componentes/Cabecalho";
 import Galeria from "../../componentes/Galeria";
 import Menu from "../../componentes/Menu";
+import Populares from "../../componentes/Populares";
 import Rodape from "../../componentes/Rodape";
 import banner from './banner.png'
 import styles from './PaginaInicial.module.scss'
 
 export default function PaginaInicial() {
 	return (
-	<>
-		<Cabecalho />
-		<main>
-			<section className={styles.principal}>
-				<Menu />
-				<div className={styles.principal__imagem}>
-					<h1>A galeria mais completa do espaço</h1>
-					<img src={banner} alt="Imagem da Terra vista do espaço" />
+		<>
+			<Cabecalho />
+			<main>
+				<section className={styles.principal}>
+					<Menu />
+					<div className={styles.principal__imagem}>
+						<h1>A galeria mais completa do espaço</h1>
+						<img src={banner} alt="Imagem da Terra vista do espaço" />
+					</div>
+				</section>
+				<div>
+					<Galeria />
+					<Populares />
 				</div>
-			</section>
-		</main>
-		<div>
-			<Galeria />
-		</div>
-		<Rodape />
-	</>
+			</main>
+			<Rodape />
+		</>
 	)
 }
